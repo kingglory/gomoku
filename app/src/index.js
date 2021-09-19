9e1f2a9063720df248183eb72c954d8dea786908
boyuan1228 <allenyby@udel.edu>
2021-09-18 20:46:15 -0400

    react, GAME Board

---
+++ app/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class Square extends React.Component {
+    render() {
+        return (
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i}/>;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            {this.renderSquare(9)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
