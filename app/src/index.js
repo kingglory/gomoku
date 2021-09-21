33ab6d17d6bcb82913db853448b47efe33a47764
boyuan1228 <allenyby@udel.edu>
2021-09-21 10:35:43 -0400

    add click function on react and fix style and color

--- app/src/index.js
+++ app/src/index.js
@@ -1,51 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
-
-
 class Square extends React.Component {
-    render() {
-        return (
-        <button className="square">
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        value: null,
+    };
+}
+
+render() {
+  return (
+      <button className="square"
+          onClick={() => this.props.onClick()}
+      >
           {this.props.value}
-        </button>
-      );
-    }
+      </button>
+  );
+}
+
+
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i}/>;
-    }
+    constructor(props) {
+      super(props);
+      this.state = {
+          squares: Array(9).fill(null),
+      };
+  }
+
+
+  renderSquare(i) {
+    return <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+    />;
+}
+
   
     render() {
-      const status = 'Next player: ';
+      const status = 'Next player: X';
   
       return (
         <div>
-          <div className="status">{status}
-          </div>
-
+          <div className="status">{status}</div>
           <div className="board-row">
+            {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-            {this.renderSquare(3)}
           </div>
-
           <div className="board-row">
+            {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
-            {this.renderSquare(6)}
           </div>
-
           <div className="board-row">
+            {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
-            {this.renderSquare(9)}
           </div>
-
         </div>
       );
     }
@@ -67,6 +82,7 @@ class Square extends React.Component {
     }
   }
   
+  // ========================================
   
   ReactDOM.render(
     <Game />,
